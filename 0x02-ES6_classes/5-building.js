ecfb8d7004b8dda29441607da09aa7222c71b523
Kymemia <kimemiachristopherr@gmail.com>
2024-07-26 16:31:48 +0300

    t.5 test 0

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,21 @@
+export default class Building {
+	constructor(sqft) {
+		this._sqft = sqft;
+	}
+
+	get sqft() {
+		return this._sqft;
+	}
+
+	set sqft(newSqft) {
+		if (typeof newSqft === 'number') {
+			this._sqft = newSqft;
+		} else {
+			throw new TypeError('sqft must be a number');
+		}
+	}
+
+	evacuationWarningMessage() {
+			throw new TypeError("Class extending Building must override evacuationWarningMessage");
+	}
+}
