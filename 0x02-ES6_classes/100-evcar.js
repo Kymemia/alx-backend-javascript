5c05951b8ee9846184825f63ef907cc83bd01174
Kymemia <kimemiachristopherr@gmail.com>
2024-07-26 21:47:33 +0300

    EVCar

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/extensions */
+import Car from './10-car.js';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  /* eslint-disable class-methods-use-this */
+  cloneCar() {
+    return new Car();
+  }
+}
