071d6e1bf22f751944f0e6f677eafe1efa701d0e
Kymemia <kimemiachristopherr@gmail.com>
2024-07-26 22:03:01 +0300

    task 6: error handling fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/extensions */
 import signUpUser from './4-user-promise.js';
-/* eslint-disable import/extensions */
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
@@ -11,5 +10,5 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
       status: result.status,
       value: result.status === 'fulfilled' ? result.value : result.reason,
     })))
-    .catch((error) => console.error('Error handling promises:', error));
+    .catch((error) => Promise.reject(new Error(`Error handling promises:, ${error.message}`));
 }
