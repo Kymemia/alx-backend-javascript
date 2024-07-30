2b459805aba62b1cb63d939d0513e00c7dc98061
Kymemia <kimemiachristopherr@gmail.com>
2024-07-30 13:50:21 +0300

    task 0 => output fix

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,17 +1,7 @@
-export class Student {
-  constructor(id, firstName, location) {
-    this.id = id;
-    this.firstName = firstName;
-    this.location = location;
-  }
-}
-
 export default function getListStudents() {
-  const students = [
+  return [
     { id: '1', firstName: 'Guillaume', location: 'San Francisco' },
     { id: '2', firstName: 'James', location: 'Columbia' },
     { id: '5', firstName: 'Serena', location: 'San Francisco' },
   ];
-
-  return students.map((student) => new Student(student.id, student.firstName, student.location));
 }
