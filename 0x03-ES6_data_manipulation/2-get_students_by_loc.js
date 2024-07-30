148eecc723c3b2fece667f427a83fa2351150c0e
Kymemia <kimemiachristopherr@gmail.com>
2024-07-30 21:43:00 +0300

    task 2. run II

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
 export default function getStudentsByLocation(city, listOfStudents) {
-  return listOfStudents.filter((student) => student.location === city);
+  return listOfStudents.filter((listOfStudents) => listOfStudents.location === city);
 }
