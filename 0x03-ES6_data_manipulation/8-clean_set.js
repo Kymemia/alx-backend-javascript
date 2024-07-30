2c778d40e51df0fb80de51e31d36eff146e34b21
Kymemia <kimemiachristopherr@gmail.com>
2024-07-31 01:29:57 +0300

    Clean set. run I: fixed handling of an empty string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(set, startString) {
-  const finalProd = [];
-  Array.from(set).forEach((value) => {
-    if (value.startsWith(startString)) {
-      finalProd.push(value.slice(startString.length));
-    }
-  });
-  return finalProd.join('-');
+  if (startString === '') {
+    return '';
+  }
+  return [...set]
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.substring(startString.length))
+    .join('-');
 }
