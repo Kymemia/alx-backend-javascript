e5a17bce2604956ff3bacf9037269cf91a3de1ef
Kymemia <kimemiachristopherr@gmail.com>
2024-08-02 20:04:56 +0300

    clean set refactoring

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,9 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') {
+  if (!set || !startString || typeof startString !== 'string' || !startString.length || set.size === 0) {
     return '';
   }
-  return [...set]
+  return Array.from(set)
     .filter((value) => value.startsWith(startString))
-    .map((value) => value.substring(startString.length))
+    .map((value) => value.slice(startString.length))
     .join('-');
 }
