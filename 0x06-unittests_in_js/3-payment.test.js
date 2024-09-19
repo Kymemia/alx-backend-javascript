14e1b2f33b23ceb06876b655819c3063e7fc3539
Kymemia <kimemiachristopherr@gmail.com>
2024-09-19 21:13:36 +0300

    spies fixx

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -15,7 +15,7 @@ describe('sendPaymentRequestToApi', function () {
   it('should call Utils.calculateNumber with SUM, 100, & 20', function () {
     const spy = sinon.spy(Utils, 'calculateNumber');
     sendPaymentRequestToApi(100, 20);
-    expect(spy.calledOnce).to.be.true;
+    expect(spy.calledOnce);
     expect(spy.calledWith('SUM', 100, 20));
     spy.restore();
   });
